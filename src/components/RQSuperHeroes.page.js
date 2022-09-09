976c7317ef4c142f3cd239e1297a00e410bf5ca0
Vukan Sukara <vukan.sukara@jsguru.io>
2022-09-09 14:14:25 +0200

    final commit

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,36 @@
-export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const RQSuperHeroesPage = () => {
+  const fetchSuperHeroes = () => {
+    return axios.get("http://localhost:4000/superheroes");
+  };
+
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes,
+    { enabled: false }
+  );
+
+  console.log({ isLoading, isFetching });
+
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
+  if (isLoading || isFetching) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch Heroes</button>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
+};
+
+export default RQSuperHeroesPage;
